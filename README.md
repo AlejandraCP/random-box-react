56da6136e4a92f1557c2367879882fa26664d672
AlejandraCP <alejandracp710@gmail.com>
2018-03-04 15:56:23 -0500

    add functionality and README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Random Box usando React
+
+## Objetivo :
+La página web cuenta con un componente cuyo contenido cambia aleatoriamente de color de fondo y tipografía.
+
+
+![Con titulo](assets/ramdongif.gif)
+
+##### Servidor de desarrollo:  
+
+Ejecutar `npm start` para un servidor de desarrollo. Navegue a `http://localhost:8080/`.
+
--- src/component/BoxContainer.js
+++ src/component/BoxContainer.js
@@ -5,4 +5,35 @@ import Box from "../actions/Box";
 
 import "../../public/build/js/index.css";
 
+const wrapper = document.getElementById("root");
 
+function randomStyle() {
+  let styles = ["font-1", "font-2", "font-3", "font-4", "font-5", "font-6"];
+  let styleRandom = styles[Math.floor(Math.random() * styles.length)];
+  return styleRandom;
+}
+
+function randomColor() {
+  const r = Math.round(Math.random() * 255);
+  const g = Math.round(Math.random() * 255);
+  const b = Math.round(Math.random() * 255);
+  const color = `rgb(${r},${g},${b})`;
+
+  return color;
+}
+
+class BoxContainer extends Component {
+  render() {
+    const colorsRandom = { backgroundColor: randomColor() };
+    return (
+      <Box
+        colorRamdon={colorsRandom}
+        fontStyle={randomStyle()}
+        phrase="Los sueños no mueren cuando abres los ojos"
+      />
+    );
+  }
+}
+
+render(<BoxContainer />, wrapper);
+export default BoxContainer;
