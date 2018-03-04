d9200e655fcb317bb45dde3dbf99b3e0535e2047
AlejandraCP <alejandracp710@gmail.com>
2018-03-04 15:29:52 -0500

    Adding structure :file_folder:

---
+++ src/component/BoxContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+import Box from "../actions/Box";
+
+import "../../public/build/js/index.css";
+
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import BoxContainer from "./component/BoxContainer";
+
+import "../public/build/js/index.css";
+
+import "bootstrap/dist/css/bootstrap.css";
