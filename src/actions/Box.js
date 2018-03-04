f58ff8ac611f903618273e7a59019c30ae39573a
AlejandraCP <alejandracp710@gmail.com>
2018-03-04 15:36:12 -0500

    Adding structure component :construction:

--- src/actions/Box.js
+++ src/actions/Box.js
@@ -2,3 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
+const Box = ({ colorRamdon, fontStyle, phrase }) => (
+  <div
+    style={colorRamdon}
+    className="position mt-5 text-center"
+  >
+    <p className={fontStyle}>{phrase}</p>
+  </div>
+);
+
+Box.propTypes = {
+  phrase: PropTypes.string.isRequired,
+};
+
+export default Box;
